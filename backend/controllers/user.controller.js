5430fa4272f2c8662ac7aec1cc70b5a52abf87a2
Seedorf Rai <dorf123see@gmail.com>
2024-07-07 13:04:26 +0545

    get carousel created for user in backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,9 +1,21 @@
+const Carousel = require("../models/carousel.model");
 const Setting = require("../models/setting.model");
 
 module.exports.setting = async function(req,res){
     try{
      const setting = await Setting.findOne();
      return res.status(200).json({setting:setting})
+    }
+    catch(err){
+        console.log(err);
+        return res.status(500),json({msg: "Internal Server Error"})
+    }
+}
+
+module.exports.getCarousel = async function(req, res){
+    try{
+     const carousel = await Carousel.find();
+     return res.status(200).json({carousels:carousel})
     }
     catch(err){
         console.log(err);
--- backend/router/user.routes.js
+++ backend/router/user.routes.js
@@ -5,4 +5,5 @@ router.get('/',(req,res)=>{
     res.status(200).json({message: "Welcome"})
 })
 router.get('/setting',userController.setting)
+router.get('/carousel',userController.getCarousel)
 module.exports = router
